5da7308436d88e39cc1e0c8b0c33309787f178c4
jorgechavezrnd <jorgechavezrnd@gmail.com>
2020-07-17 21:48:47 -0400

    Pruebas unitarias y de intengracion, funcionando

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,22 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
+import { RouterTestingModule } from '@angular/router/testing';
+import { By } from '@angular/platform-browser';
+import { RouterOutlet, RouterLinkWithHref } from '@angular/router';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+
 describe('AppComponent', () => {
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
+      imports: [
+        RouterTestingModule.withRoutes([])
+      ],
+      schemas: [ NO_ERRORS_SCHEMA ]
     }).compileComponents();
   }));
 
@@ -22,10 +32,14 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('pruebas');
   });
 
-  it('should render title', () => {
+  it('Debe de tener un router-outlet', () => {
+
     const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('pruebas app is running!');
+
+    const debugElement = fixture.debugElement.query( By.directive( RouterOutlet ) );
+
+    expect( debugElement ).not.toBeNull();
+
   });
+
 });
